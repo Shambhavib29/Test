48e033c3d36c0928263c7c3758542988a7307a39
Shambhavib29 <shambhavibhardwaj29@gmail.com>
2024-01-30 23:30:40 +0530

    water

--- hungry.py
+++ hungry.py
@@ -2,5 +2,10 @@
 hungry=input("are you hungry?")
 if hungry == "yes":
     print("eat samosa")
+    print("eat pizza")
+    print("eat burger")
 else:
+    thirsty=input("are you thirsty?")
+    if thirsty =="yes":
+        print("drink water")
     print("do your homework")
